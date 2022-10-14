77e517bf787100159e000597c7e79f8c594fc971
ceciliatruong <114256675+ceciliatruong@users.noreply.github.com>
2022-10-13 18:53:57 -0700

    commit

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 class Handler implements URLHandler {
@@ -9,10 +10,9 @@ class Handler implements URLHandler {
         if (url.getPath().contains("/search")) {
             String[] parameters = url.getQuery().split("=");
             if(parameters[0].equals("s")) {
-                String[] newList = list.toArray(new String[list.size()]);
-                for(int i = 0; i < newList.length; i++) {
-                    if(newList[i].contains(parameters[2]) == true) {
-                        finalSearch.add(newList[i]);
+                for(int i = 0; i < list.size(); i++) {
+                    if(list.get(i).contains(parameters[2]) == true) {
+                        finalSearch.add(list.get(i));
                     }
                 }
                 if(finalSearch.size() <= 0) {
@@ -20,7 +20,9 @@ class Handler implements URLHandler {
                 }
                 else {
                     String finalList = finalSearch.toString();
-                    return String.format("Here are the following matches:", finalList);
+                    String[] finalTest = finalSearch.toArray(new String[finalSearch.size()]);
+                    System.out.println(finalList);
+                    return ("Here are the following matches:");
                 }
             }
         }
@@ -29,10 +31,11 @@ class Handler implements URLHandler {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
                 list.add(parameters[1]);
-                return("String has been successfully entered into the search engine!");
+                String[] test = list.toArray(new String[list.size()]);
+                return("String has been successfully entered into the search engine! This is the current search engine word bank" + Arrays.toString(test));
                 }
             }   
-        return "welcome to cc's search engine!!! type /add?s=[enter any word here] after the url to add some terms to the search engine & type /search?s=[enter any word here] after the url to see if your words match up with the words inputted in the search engine!";  
+        return ("welcome to cc's search engine!!! type /add?s=[enter any word here] after the url to add some terms to the search engine & type /search?s=[enter any word here] after the url to see if your words match up with the words inputted in the search engine!");  
     }
 }
 
