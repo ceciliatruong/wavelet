741e91c832517b9a34e47d60621ac10ab38d56d3
ceciliatruong <114256675+ceciliatruong@users.noreply.github.com>
2022-10-13 19:19:42 -0700

    commit

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,17 +1,17 @@
 import java.io.IOException;
 import java.net.URI;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
-    LinkedList<String> list = new LinkedList<String>();
-    LinkedList<String> finalSearch = new LinkedList<String>();
+    ArrayList<String> list = new ArrayList<String>();
+    ArrayList<String> finalSearch = new ArrayList<String>();
     public String handleRequest(URI url) {
         if (url.getPath().contains("/search")) {
             String[] parameters = url.getQuery().split("=");
             if(parameters[0].equals("s")) {
                 for(int i = 0; i < list.size(); i++) {
-                    if(list.get(i).contains(parameters[2]) == true) {
+                    if(list.get(i).contains(parameters[1]) == true) {
+                        System.out.println(list.get(i));
                         finalSearch.add(list.get(i));
                     }
                 }
@@ -20,9 +20,7 @@ class Handler implements URLHandler {
                 }
                 else {
                     String finalList = finalSearch.toString();
-                    String[] finalTest = finalSearch.toArray(new String[finalSearch.size()]);
-                    System.out.println(finalList);
-                    return ("Here are the following matches:");
+                    return ("Here are the following matches:" + finalList);
                 }
             }
         }
@@ -31,8 +29,8 @@ class Handler implements URLHandler {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
                 list.add(parameters[1]);
-                String[] test = list.toArray(new String[list.size()]);
-                return("String has been successfully entered into the search engine! This is the current search engine word bank" + Arrays.toString(test));
+                String tempList = list.toString();
+                return("String has been successfully entered into the search engine! This is the current search engine word bank" + tempList);
                 }
             }   
         return ("welcome to cc's search engine!!! type /add?s=[enter any word here] after the url to add some terms to the search engine & type /search?s=[enter any word here] after the url to see if your words match up with the words inputted in the search engine!");  
